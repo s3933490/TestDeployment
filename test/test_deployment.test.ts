b19998d62280cbbcc86aa2505ef17c10dde89f8c
Jacob Daniels <s3933490@student.rmit.edu.au>
2025-03-16 13:38:24 +0800

    Initial commit

---
+++ test/test_deployment.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TestDeployment from '../lib/test_deployment-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/test_deployment-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TestDeployment.TestDeploymentStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
